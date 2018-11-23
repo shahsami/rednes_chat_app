8253191948bb1d1f1888c57a69d75cf6fdee7ed0
SHAH MD. ABDUS SALAM MOHIUDDIN <shah.sami77@gmail.com>
2018-11-23 06:25:47 +0600

    storing adding and removing chatter

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var messages =[];
 var redis = require('redis');
-var client = redis.createClient();
+var redisClient = redis.createClient();
 var storeMessage = function(name, data){
-	messages.push({name: name, data: data}); //add message to end of array
-	if (messages.length > 10) {
-		messages.shift(); // if more than 10 message long, remove the first one
-	}
+	// messages.push({name: name, data: data}); //add message to end of array
+	// if (messages.length > 10) {
+	// 	messages.shift(); // if more than 10 message long, remove the first one
+	// }
+	var message = JSON.strigify({name: name, data: data}); // trun object into string to store in redis
+
+	redisClient.lpush("messages", message, function(err, response) {
+		redisClient.ltrip("messages", 0, 9); // to keep newest 10 items
+	});
 }
 
 io.on('connection', function(client) {
@@ -23,11 +28,41 @@ io.on('connection', function(client) {
 		client.broadcast.emit("chat", name + " joined the chat");
 
 			//client.nickname = name; // set the nickname associated with the client
+		redisClient.lrange("messages", 0, -1, function(err, messages){
+			messages = messages.reverse(); // reverse to emit in correct order
+		});
+
+
 		//iterate through message array and emit a message on the connceting client for each one
 		messages.forEach(function(message) {
+			message = JSON.parse(message); // parse into JSON object
 			client.emit("messages", message.name + ": " + message.data);
 		});
+
+		client.broadcast.emit("add chatter", name); //notify other clients a chatter has joined
+
+		// emit all the currently logged in chatter to the newly connected client
+		redisClient.smembers('chatters', function(err, names) {
+			console.log(names);
+			names.forEach(function(name) {
+				client.emit('add chatter', name);
+			});
 		});
+
+
+
+		redisClient.sadd("chatters", name); // add name to redis chatter set
+		});
+
+		// removing chatters
+		client.on('disconnect', function(name) {
+		client.get('nickname', function(err, name) {
+			console.log('++chatter leaved : ' + name);
+			client.broadcast.emit('remove chatter', name);
+
+			redisClient.srem('chatters', name);	
+		});
+	});
 
 	client.on('messages', function(message) { //listen for messages e
 		//var nickname = client.nickname; //get the nickname of the client
@@ -43,6 +78,7 @@ io.on('connection', function(client) {
 	//client.emit('messages', {hello:'hello you!'});
 });
 
+// serve index.html
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
